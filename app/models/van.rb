458bddb37a552d19b71c7978fa26c25d79a20351
Anibal Flores <anibal.utem@gmail.com>
2015-12-09 11:49:50 -0300

    vistas listas

--- app/models/van.rb
+++ app/models/van.rb
@@ -4,14 +4,12 @@ class Van < ActiveRecord::Base
 	validates :capacidad, presence: true
   validates :patente, presence: true
   validates :patente, uniqueness: {case_sensitive: false ,message: "ya existe"}
-  validates_numericality_of :kilimetrage, greater_than_or_equal_to: 0,
-                            :message => "Debe ser positivo"
-  validates_numericality_of :capacidad, greater_than: 0,
+    validates_numericality_of :capacidad, greater_than: 0,
                             :message => "Debe ser mayor a 0"
   validates_numericality_of :capacidad,less_than_or_equal_to:30,
                             :message => "Parece ser muy grande"
-  validates_format_of :patente, 
+  validates_format_of :patente,
                       :with => /\A[a-z]{2}[\.\- ]?[0-9]{2}[\.\- ]?[0-9]{2}|[b-d,f-h,j-l,p,r-t,v-z]{2}[\-\. ]?[b-d,f-h,j-l,p,r-t,v-z]{2}[\.\- ]?[0-9]{2}\Z/i,
                       :message => "Formato no Valido"
-   
+
 end
